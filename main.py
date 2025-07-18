98c10a6f13ef2932815ae3ded737e288d8b286a4
Nick🛰️ <cpcodex23@gmail.com>
2025-07-18 09:31:02 -0400

    update

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import csv as csv
 
+# csv file name
+CSV_FILE = "expense.csv"
+
 
 def sep():
     print('=' * 65)
@@ -10,16 +13,27 @@ def setup_csv():
 
 
 def add_expense():
-    pass
+    with open(CSV_FILE, 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=' ',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
+        spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
 
 
 def view_expense():
-    pass
+    with open(CSV_FILE, newline='') as f:
+
+        exp_reader = csv.reader(f, delimiter=' ', quotechar='|')
+
+    for row in exp_reader:
+
+        print(', '.join(row))
 
 
 def main():
+    add_expense()
     sep()
-    print('--- Test complete ---')
+    print('--- Task Complete ---')
     sep()
 
 
